6401dc670dd243d9c50ab4e987f9e73a3ea4f622
jmei2 <jmei2@uchicago.edu>
2024-04-27 11:25:35 -0500

    wip: camera up

--- color-assistant/App.js
+++ color-assistant/App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import React from 'react';
 
 export default function App() {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  if (!permission) {
+    return <View />;
+  }
+  
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center'}}>
+          Enable camera permissions for this app to start identifying colors!
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Camera 
+        style={{width: "75%", height: "75%"}}
+        type={CameraType.back} 
+      ></Camera>
+      <View>
+        <Text style={styles.textContainer}>Start by taking a photo of your garment!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +40,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '50px',
   },
 });
