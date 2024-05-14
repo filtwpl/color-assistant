49313c19a40aa53f9750abc4fd6e497a83cb0f2c
Kathy Yao <kathy@Kathys-Mac.local>
2024-05-13 23:26:21 -0500

    add firebase

--- color-assistant/ios/colorassistant/AppDelegate.mm
+++ color-assistant/ios/colorassistant/AppDelegate.mm
@@ -2,6 +2,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
+#import <Firebase.h>
+
+@import UIKit;
+@import FirebaseCore;
+
 
 @implementation AppDelegate
 
@@ -12,9 +17,11 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  [FIRApp configure];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
